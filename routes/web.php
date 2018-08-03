a0d3667ccdfedd5ac471253a0d5bf4db96b0f6e4
Kazushi Takamura <sakamatapd5@gmail.com>
2018-08-03 17:56:36 +0900

    Add:  管理画面ログイン認証追加

--- routes/web.php
+++ routes/web.php
@@ -20,9 +20,11 @@ Auth::routes();
 // デフォルトのログイン直後画面 You are logged in!
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/admin_users_edit', 'AdminUserController@index');
-Route::get('/admin_mac_address', 'AdminMacAddressController@index');
-Route::get('/admin_mac_address/edit{id?}', 'AdminMacAddressController@edit');
+// 管理画面 認証済みuserのみ表示
+Route::get('/admin_users_edit', 'AdminUserController@index')->middleware('auth');
+Route::get('/admin_mac_address', 'AdminMacAddressController@index')->middleware('auth');
+Route::post('/admin_mac_address/update', 'AdminMacAddressController@update')->middleware('auth');
+Route::get('/admin_mac_address/edit{id?}', 'AdminMacAddressController@edit')->middleware('auth');
 
 // 外部からのPOST受け取り先 csrf off
 Route::post('/inport_post/mac_address', 'InportPostController@MacAddress');
