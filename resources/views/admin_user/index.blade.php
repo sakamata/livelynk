157bc27b6d6b8ed47df3bec352ad5d42272d081e
Kazushi Takamura <sakamatapd5@gmail.com>
2018-08-07 03:24:58 +0900

    Add view:  ヘッダー部分に遷移ボタン追加

--- resources/views/admin_user/index.blade.php
+++ resources/views/admin_user/index.blade.php
@@ -1,6 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
+@component('components.header_menu')
+@endcomponent
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-12">
---
+++ resources/views/components/header_menu.blade.php
@@ -0,0 +1,8 @@
+<div class="container">
+    <div class="row">
+        <div class="col-md-12" style="text-align:right; padding-bottom:15px;">
+            <a href="/admin_mac_address" class="btn btn-info" role="button">MAC Address一覧</a>
+            <a href="/admin_user" class="btn btn-info" role="button">ユーザー一覧</a>
+        </div>
+    </div>
+</div>
