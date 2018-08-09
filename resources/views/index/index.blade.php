6e46f3f94f9cf964b80f20c5afc9987395387c73
Kazushi Takamura <sakamatapd5@gmail.com>
2018-08-10 02:05:40 +0900

    Add: index/index タイトル追加

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -7,7 +7,7 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header"><h2>Who's There?</h2></div>
+                <div class="card-header"><h2>Who's There? @ Geek Office Ebisu</h2></div>
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
