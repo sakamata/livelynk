8ed8fa68ee5e6c7bca3df35ffd8cbc396e9ba730
Kazushi Takamura <sakamatapd5@gmail.com>
2018-08-10 00:38:44 +0900

    Bugfix:  本番環境のMySQL仕様の違いによるmigrateエラーの対応　Email等のカラムの文字数制限を255から191に変更

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // マイグレーション時の Syntax error対策
+        // 本番環境のMySQLのバージョンが古い為これより大きなカラム数に対応できない為
+        // unique email 等の長さが MAX 255 -> 191となる
+        Schema::defaultStringLength(191);
     }
 
     /**
