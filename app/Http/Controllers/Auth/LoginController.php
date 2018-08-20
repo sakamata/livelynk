4d982e56d93b7fcf3e52eaccffec1ac347b85c4e
Kazushi Takamura <sakamatapd5@gmail.com>
2018-08-20 19:18:37 +0900

    Add:  Logout処理追加、index画面にリダイレクト

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -36,6 +37,12 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
         $this->redirectTo = env("INDEX_PATH");
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return redirect(env("INDEX_PATH"));
     }
 
 }
