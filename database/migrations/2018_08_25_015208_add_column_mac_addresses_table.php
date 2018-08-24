98df2ad4dc887ae631b6c6071b0465ff0eefab04
Kazushi Takamura <sakamatapd5@gmail.com>
2018-08-25 02:09:58 +0900

    Add: migrationファイル追加　mac_addresses tableに最終投稿時間 posted_at カラム追加

---
+++ database/migrations/2018_08_25_015208_add_column_mac_addresses_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumnMacAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('mac_addresses', function (Blueprint $table) {
+            $table->timestamp('posted_at')->nullable()->after('departure_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('mac_addresses', function (Blueprint $table) {
+            $this->dropColumn('posted_at');
+        });
+    }
+}
