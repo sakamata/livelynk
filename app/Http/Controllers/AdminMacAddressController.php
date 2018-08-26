096cf21f66378167f44fc3af22bf39c800a25bc6
Kazushi Takamura <sakamatapd5@gmail.com>
2018-08-26 18:00:56 +0900

    Edit:  MACAddress一覧画面のソート変更

--- app/Http/Controllers/AdminMacAddressController.php
+++ app/Http/Controllers/AdminMacAddressController.php
@@ -12,7 +12,8 @@ class AdminMacAddressController extends Controller
 {
     public function index(Request $request)
     {
-        $items = 'App\MacAddress'::orderBy('updated_at', 'desc')->get();
+        $items = 'App\MacAddress'::orderBy('hide', 'asc')
+            ->orderBy('arraival_at', 'desc')->get();
         return view('admin_mac_address.index', [
             'items' => $items,
         ]);
