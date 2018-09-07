1c4db1f1bd4638217450d3b8fa48e3f0a2312702
Kazushi Takamura <sakamatapd5@gmail.com>
2018-09-07 14:24:04 +0900

    Edit:  ヘッダーメニューのスタイル調整

--- resources/views/components/header_menu.blade.php
+++ resources/views/components/header_menu.blade.php
@@ -1,11 +1,11 @@
 @auth
 <div class="container">
     <div class="row">
-        <div class="col-md-12" style="text-align:right; padding-bottom:15px;">
-            <a href="{{ env("INDEX_PATH") }}" class="btn btn-info" role="button">HOME</a>
-            <a href="/admin_mac_address" class="btn btn-info" role="button">MAC Address一覧</a>
-            <a href="/admin_user" class="btn btn-info" role="button">ユーザー一覧</a>
-            <a href="/admin_user/add" class="btn btn-info" role="button">新規ユーザー作成</a>
+        <div class="col-md-12" style="text-align:right;">
+            <a href="{{ env("INDEX_PATH") }}" class="btn btn-info" role="button" style="margin-bottom:10px;">HOME</a>
+            <a href="/admin_mac_address" class="btn btn-info" role="button" style="margin-bottom:10px;">MAC Address一覧</a>
+            <a href="/admin_user" class="btn btn-info" role="button" style="margin-bottom:10px;">ユーザー一覧</a>
+            <a href="/admin_user/add" class="btn btn-info" role="button" style="margin-bottom:10px;">新規ユーザー作成</a>
         </div>
     </div>
 </div>
