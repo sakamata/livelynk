a9d28a3cd1fb0a5a2b0daa97da13d227bfb268a5
Kazushi Takamura <sakamatapd5@gmail.com>
2018-10-08 20:45:55 +0900

    Add: パスワードリセットメール、日本語と独自テンプレートを追加

--- app/User.php
+++ app/User.php
@@ -5,6 +5,8 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
+use App\Notifications\PasswordResetNotification;
+
 class User extends Authenticatable
 {
     use Notifiable;
@@ -26,4 +28,16 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+    * パスワードリセット通知の送信をオーバーライド
+    *
+    * @param  string  $token
+    * @return void
+    */
+    public function sendPasswordResetNotification($token)
+    {
+        $this->notify(new PasswordResetNotification($token));
+    }
+
 }
