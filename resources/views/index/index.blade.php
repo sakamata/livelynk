01a1929e35f802add2d38d65aa765bd20265ccac
vagrant <vagrant@homestead>
2018-10-11 06:38:29 +0000

    Edit: home のデザイン変更

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -14,7 +14,7 @@
 <div class="comp-box-container clearfix">
 @php $i = 0; @endphp
 @foreach ($items as $item)
-  <div class="comp-box">
+  <div class="comp-box clearfix">
     <div class="name">
       <div class="icon">
         <i class="fas fa-user-circle"></i>
@@ -22,12 +22,11 @@
       <div class="text">{{$item->vendor}}</div>
     </div>
     <div class="arrival">
-      <span class="time-head">IN</span>
-      <span class="time-body">{{date('n/j G:i', strtotime($item->arraival_at))}}</span>
-      <!-- 在席の可能性をパーセンテージで表す値 -->
-      <span class="time-accuracy">{{ $rate[$i] }}</span>
+      <div class="head">IN</div>
+      <div class="time">{{date('n/j G:i', strtotime($item->arraival_at))}}</div>
+      <div class="accuracy">{{ $rate[$i] }}</div>
     </div>
-    <div class="flag">
+    <div class="flag sp-none">
       <img src="{{asset("img/icon/newcomer.png")}}" width="46"  alt="Newcomer!">
     </div>
   </div>
@@ -35,7 +34,7 @@
 @endforeach
 @php $i = 0; @endphp
 @foreach ($items1 as $item)
-  <div class="comp-box">
+  <div class="comp-box clearfix">
     <div class="name">
       <div class="icon">
         <i class="fas fa-user-circle"></i>
@@ -43,28 +42,27 @@
       <div class="text">{{$item->name}}</div>
     </div>
     <div class="arrival">
-      <span class="time-head">IN</span>
-      <span class="time-body">{{date('n/j G:i', strtotime($item->max_arraival_at))}}</span>
-      <!-- 在席の可能性をパーセンテージで表す値 -->
-      <span class="time-accuracy">{{ $rate1[$i] }}</span>
+      <div class="head">IN</div>
+      <div class="time">{{date('n/j G:i', strtotime($item->max_arraival_at))}}</div>
+      <div class="accuracy">{{ $rate1[$i] }}</div>
     </div>
-    <div class="flag">
+    <div class="flag sp-none">
       <img src="{{asset("img/icon/im_here.png")}}" width="46"  alt="I'm here!">
     </div>
   </div>
   @php $i++; @endphp
 @endforeach
 @foreach ($items2 as $item)
-  <div class="comp-box absence">
+  <div class="comp-box clearfix absence">
     <div class="name">
       <div class="icon">
         <i class="fas fa-user-circle"></i>
       </div>
       <div class="text">{{$item->name}}</div>
     </div>
-    <div class="depature">
-      <span class="time-head">OUT</span>
-      <span class="time-body">{{date('n/j G:i', strtotime($item->last_access))}}</span>
+    <div class="arrival">
+      <div class="head">OUT</div>
+      <div class="time">{{date('n/j G:i', strtotime($item->last_access))}}</div>
     </div>
   </div>
 @endforeach
