a4733c199a5059a1e75fbc2ff7491a6bbcf2715f
Kazushi Takamura <sakamatapd5@gmail.com>
2018-11-16 13:35:52 +0900

    Edit: AuthUser Modelに $hidden の値追加 JSON取得の際隠すべき値を設定

--- app/AuthUser.php
+++ app/AuthUser.php
@@ -17,6 +17,15 @@ class AuthUser extends Authenticatable
      */
     protected $table = 'auth_users';
 
+    /**
+     * The attributes that should be hidden for arrays.
+     * JSON出力の際誤って含めいない属性という事らしい
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
     // 日時表記変更の ->format('Y-m-d') を使いたいカラム名を指定する
     protected $dates = [
         'last_access',
