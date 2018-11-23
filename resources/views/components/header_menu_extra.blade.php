bca931e4de974d4671dd42bead246b12b88d919e
Kazushi Takamura <sakamatapd5@gmail.com>
2018-11-24 03:45:43 +0900

    Edit: GeekOfficwWebサービスへのリンクを他のコミュニティでは非表示に変更

--- resources/views/components/header_menu_extra.blade.php
+++ resources/views/components/header_menu_extra.blade.php
@@ -1,2 +1,4 @@
+@if($community->id == 1)
 <a href="https://geekoffice.herokuapp.com" target="_blank">ギークオフィスWebサービス</a>
+@endif
 <a href="https://tumolink.herokuapp.com/home" target="_blank">ツモリンク</a>
