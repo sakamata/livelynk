bd155e53f3d5e2b8010c08b2a87e00ddb65cadcc
Kazushi Takamura <sakamatapd5@gmail.com>
2018-12-08 04:30:36 +0900

    Add:  ***migrate*** communitiesにGoogleHome設定用フィールド追加

--- database/migrations/2018_12_06_210145_edit_communities_table_2018_12_06.php
+++ database/migrations/2018_12_06_210145_edit_communities_table_2018_12_06.php
@@ -15,8 +15,9 @@ class EditCommunitiesTable20181206 extends Migration
     {
         Schema::table('communities', function (Blueprint $table) {
             $table->text('service_name')->nullable(false)->change();
+            $table->text('google_home_name')->nullable()->after('ifttt_webhooks_key');
+            $table->text('google_home_mac_address')->nullable()->after('google_home_name');
         });
-
     }
 
     /**
@@ -28,6 +29,8 @@ class EditCommunitiesTable20181206 extends Migration
     {
         Schema::table('communities', function (Blueprint $table) {
             $table->text('service_name')->nullable()->change();
+            $table->dropColumn('google_home_name');
+            $table->dropColumn('google_home_mac_address');
         });
     }
 }
