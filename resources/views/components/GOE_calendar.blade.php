bdfde58fbf485e6529be151c3084a2155d4ffef6
Kazushi Takamura <sakamatapd5@gmail.com>
2019-02-12 20:13:03 +0900

    Edit:  file_get_contents が取得できなかった場合errorとなったのを修正

--- resources/views/components/GOE_calendar.blade.php
+++ resources/views/components/GOE_calendar.blade.php
@@ -1,15 +1,22 @@
 @php
 // viewに直接書いているのは後々共通処理化する予定です。今は特別に処理してます。
-!env('GOE_EVENT_URL') ? $events="" : $events = file_get_contents(env('GOE_EVENT_URL'));
+$events = "";
+if(!env('GOE_EVENT_URL')) {
+    $events = file_get_contents(env('GOE_EVENT_URL'));
+}
 $esc_tags =array('&lt;br&gt;', '&lt;div&gt;', '&lt;/div&gt;');
 $tags =array('<br>', '<div>', '</div>');
-$events_escaped = str_replace($esc_tags, $tags, htmlspecialchars( $events ,ENT_QUOTES) );
+if (!$events) {
+    $events_escaped = "";
+} else {
+    $events_escaped = str_replace($esc_tags, $tags, htmlspecialchars( $events ,ENT_QUOTES) );
+}
 @endphp
 
 @if($community->id == 1)
 <p>今日のイベント</p>
     @if( strlen($events_escaped) > 11 )
-    <div>{!! $events_escaped !!}</div>
+    <div>{ $events_escaped }</div>
     @else
     <div>予定はありません</div>
     @endif
