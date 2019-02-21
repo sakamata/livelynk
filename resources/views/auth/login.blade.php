ccc11b18169ea68299b46ebaec31d5e11d055f4e
Kazushi Takamura <sakamatapd5@gmail.com>
2019-02-22 02:03:10 +0900

    Edit: ログイン画面、ログインを維持するのチェックのONをデフォルトに変更

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -44,7 +44,7 @@
                         <div class="form-group row">
                             <div class="col-md-6 offset-md-4">
                                 <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }} checked="checked">
 
                                     <label class="form-check-label" for="remember">
                                         {{ __('auth.Remember Me') }}
