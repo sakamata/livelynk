a6cbf95252636e850ba13b8be11019434caeae95
Kazushi Takamura <sakamatapd5@gmail.com>
2019-02-26 14:33:57 +0900

    Add:  **migrate** auth_users viewにname_reading追加

---
+++ database/migrations/2019_02_26_133654_edit_view_auth_users_table_2019_02_26.php
@@ -0,0 +1,58 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class EditViewAuthUsersTable20190226 extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // users.name_reading 追加
+        DB::statement('DROP VIEW IF EXISTS auth_users');
+        DB::statement("
+            CREATE VIEW auth_users AS
+            SELECT
+                community_user.id,
+                community_user.user_id,
+                community_user.community_id,
+                users.name,
+                users.name_reading,
+                users.unique_name,
+                users.email,
+                users.provisional,
+                users.facebook_id,
+                users.password,
+                users.remember_token,
+                communities.user_id AS reader_id,
+                communities.name AS community_unique_name,
+                communities.service_name,
+                communities_users_statuses.role_id,
+                communities_users_statuses.hide,
+                communities_users_statuses.last_access,
+                communities_users_statuses.created_at,
+                communities_users_statuses.updated_at,
+                roles.role
+            FROM community_user
+            JOIN users ON (community_user.user_id = users.id)
+            JOIN communities ON (community_user.community_id = communities.id)
+            JOIN communities_users_statuses ON (community_user.id = communities_users_statuses.id)
+            JOIN roles ON (roles.id = communities_users_statuses.role_id)
+        ");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::statement('DROP VIEW IF EXISTS auth_users');
+    }
+}
