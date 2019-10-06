2330d0c7d3684ec35863f43be6ecf451f9129afe
Kazushi Takamura <sakamatapd5@gmail.com>
2019-10-06 19:40:17 +0900

    Edit: iphoneで編集したcssが読み込まれない可能性があるためファイル名を一時的に変更

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,7 +32,7 @@
     <link href="{{ asset('css/app.css') }}?q={{$file_date}}" rel="stylesheet">
     <link href="{{ asset('css/livelynk.css') }}?q={{$file_date}}" rel="stylesheet">
     @if(Request::is('/') || Request::is('index'))
-    <link href="{{ asset('css/tumolink.css') }}?q={{$file_date}}" rel="stylesheet">
+    <link href="{{ asset('css/tumolink_.css') }}?q={{$file_date}}" rel="stylesheet">
     <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet" type="text/css">
     @endif
 
